7145c20cbc39511252a37c31f19fc6f2999cb8d4
Sangram4213 <92009582+Sangram4213@users.noreply.github.com>
2024-09-09 17:03:30 +0530

    add signUpUser controller function

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,16 +1,59 @@
-export const logInUser =async(req,res)=>{
-   try {
-    const {fullName,userName,password,confirmPassword,gender} = req.body;
+import User from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+
+export const logInUser = async (req, res) => {
+  
+};
+
+export const signUpUser = async(req, res) => {
+    try {
+        const { fullName, userName, password, confirmPassword, gender } = req.body;
     
-   } catch (error) {
-     
-   }
-}
+        if (password !== confirmPassword) {
+          return res.status(400).json({
+            error: "Password don't match",
+          });
+        }
+    
+        const user = await User.findOne({ userName });
+    
+        if (user)
+          return res.status(400).json({
+            error: "Username already exist",
+          });
+    
+        //Hash Password Here
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password,salt);
+    
+        const boyProfile = `https://avatar.iran.liara.run/public/boy?username=${userName}`;
+        const girlProfile = `https://avatar.iran.liara.run/public/girl?username=${userName}`;
 
-export const signUpUser =(req,res)=>{
-    console.log("Signin");
-}
+        // Generate JWT token here
+    
+        const newUser = await User.create({
+          fullName,
+          userName,
+          password:hashPassword,
+          gender,
+          profilePic: gender === "male" ? boyProfile : girlProfile,
+        });
+    
+        res.status(201).json({
+          _id: newUser._id,
+          fullName: newUser.fullName,
+          userName: newUser.userName,
+          gender: newUser.gender,
+          profilePic: newUser.profilePic,
+        });
+      } catch (error) {
+        console.log(error);
+        res.status(500).json({
+          error: "Internal Server Error",
+        });
+      }
+};
 
-export const logOutUser =(req,res)=>{
-    console.log("LogOut");
-}
+export const logOutUser = (req, res) => {
+  console.log("LogOut");
+};
