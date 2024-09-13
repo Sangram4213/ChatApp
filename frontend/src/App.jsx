58cb2b42226c156efad1b81b5bc5a72bed04df29
Sangram4213 <92009582+Sangram4213@users.noreply.github.com>
2024-09-13 19:34:36 +0530

    Create Authentication context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,28 @@
-import { Route,Routes } from 'react-router-dom';
-import Home from './pages/home/home';
-import Login from './pages/login/login';
-import SignUp from './pages/signup/signup';
-import {Toaster} from 'react-hot-toast'
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/home";
+import Login from "./pages/login/login";
+import SignUp from "./pages/signup/signup";
+import { Toaster } from "react-hot-toast";
+import { useAuthContext } from "./context/AuthContext";
 
 function App() {
+  const { authUser } = useAuthContext();
+
   return (
     <>
-      <div className='p-4 h-screen flex items-center justify-center'>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/signup' element={<SignUp/>}/>
-      </Routes>
-      <Toaster/>
+      <div className="p-4 h-screen flex items-center justify-center">
+        <Routes>
+          <Route path="/" element={authUser ? <Home/> : <Navigate to={"/login"} />} />
+          <Route
+            path="/login"
+            element={authUser ? <Navigate to={"/"} /> : <Login />}
+          />
+          <Route
+            path="/signup"
+            element={authUser ? <Navigate to={"/"} /> : <SignUp />}
+          />
+        </Routes>
+        <Toaster />
       </div>
     </>
   );
--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
 
 const useSignup = () => {
   const [loading, setLoading] = useState(false);
+  const {setAuthUser}=useAuthContext()
 
   const signup = async ({
     fullName,
@@ -36,6 +38,12 @@ const useSignup = () => {
       });
 
       const data = await res.json();
+
+      if(data.error) throw new Error(data.error);
+
+      localStorage.setItem("chart-user",JSON.stringify(data));
+
+      setAuthUser(data);
 
       console.log(data);
     } catch (error) {
